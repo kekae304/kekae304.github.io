44b8875b46d0dd65e6a025b0bc0fad9abe2ddcdf
kekae304 <122228402+kekae304@users.noreply.github.com>
2025-01-19 20:46:39 +0000

    Deploying to gh-pages from @ kekae304/kekae304.github.io@4389ee095cf3758ae126d39f9dd797f13efeea0b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -78,9 +78,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-stack-n-slice",
+          title: 'Stack N Slice',
+          description: "The ultimate sandwich maker.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
